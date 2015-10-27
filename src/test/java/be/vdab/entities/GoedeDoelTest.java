8ae443f8c4d0d67c34f89cb4e4ba5f2fc23808bb
sammyangels <sammyangels@gmail.com>
2015-10-27 10:24:04 +0100

    5.1 Falende test toegevoegd

--- src/test/java/be/vdab/entities/GoedeDoelTest.java
+++ src/test/java/be/vdab/entities/GoedeDoelTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class GoedeDoelTest {
 	private static final String NAAM ="CliniClowns"; 
 	private GoedeDoel doel;
@@ -17,4 +19,8 @@ public class GoedeDoelTest {
 	public void getNaam() {
 		assertEquals(NAAM, doel.getNaam());
 	}
+    @Test
+    public void eenNieuwDoelHeeftNogNietsOpgebracht() {
+        assertEquals(0, doel.getOpgebracht().compareTo(BigDecimal.ZERO));
+    }
 }
